73f6f550c47d5e4c031edef963488e2ec1021af5
Alex <alexsavrasov@hotmail.com>
2022-08-21 19:51:19 +0300

    Not optimal solution for 7. yet :(

--- 7. Container with Most Water.py
+++ 7. Container with Most Water.py
@@ -6,19 +6,27 @@ class Solution:
         sqs = []
         for i, k in enumerate(height):
             for j, l in enumerate(reversed(height)):
-                if l >= k and j+1 != len(height) - i:
+                if l < k or len(height) < j+i+1:
+                    pass
+                else:
                     new_sq_1 = min(k, l) * abs(len(height) - j - 1 - i)
                     break
-                else:
-                    pass
             for j, l in enumerate(height):
-                if l >= k and i != j:
+                if l < k or i < j:
+                    pass
+                else:
+                    print('Вхят i: ' + str(i) + ' k: ' + str(k) + ' j: ' + str(j) + ' l: ' + str(l))
                     new_sq_2 = min(k, l) * abs(i - j)
                     break
-                else:
-                    pass
             sqs.append(max(new_sq_1, new_sq_2))
         return max(sqs)
 # TEST
 s = Solution()
-print(s.maxArea([1, 0, 0, 0, 0, 0, 0, 7, 8]))
+print(s.maxArea([2,3,4,5,18,17,6]))
+
+
+i = 4
+k = 18
+
+j = 1
+l = 17
