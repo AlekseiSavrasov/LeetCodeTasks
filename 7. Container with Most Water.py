ef95a53f7664044fefa6126cb89a3ffc65c26ed9
Alex <alexsavrasov@hotmail.com>
2022-08-21 19:51:43 +0300

    Not optimal solution for 7. yet :(

--- 7. Container with Most Water.py
+++ 7. Container with Most Water.py
@@ -15,7 +15,6 @@ class Solution:
                 if l < k or i < j:
                     pass
                 else:
-                    print('Вхят i: ' + str(i) + ' k: ' + str(k) + ' j: ' + str(j) + ' l: ' + str(l))
                     new_sq_2 = min(k, l) * abs(i - j)
                     break
             sqs.append(max(new_sq_1, new_sq_2))
@@ -23,10 +22,3 @@ class Solution:
 # TEST
 s = Solution()
 print(s.maxArea([2,3,4,5,18,17,6]))
-
-
-i = 4
-k = 18
-
-j = 1
-l = 17
