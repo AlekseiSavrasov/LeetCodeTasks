83684d783fa28a791f5e64a10e9730789f2eb34d
Alex <alexsavrasov@hotmail.com>
2022-08-28 14:28:32 +0300

    The task is done!

--- 7. Container with Most Water.py
+++ 7. Container with Most Water.py
@@ -1,44 +1,24 @@
-# SOLUTION: It works, but not optimal
-# class Solution:
-#     def maxArea(self, height: int) -> int:
-#         new_sq_1 = 0
-#         new_sq_2 = 0
-#         sqs = []
-#         for i, k in enumerate(height):
-#             for j, l in enumerate(reversed(height)):
-#                 if l < k or len(height) < j+i+1:
-#                     pass
-#                 else:
-#                     new_sq_1 = min(k, l) * abs(len(height) - j - 1 - i)
-#                     break
-#             for j, l in enumerate(height):
-#                 if new_sq_1 > 0 or l < k or i < j:
-#                     pass
-#                 else:
-#                     new_sq_2 = min(k, l) * abs(i - j)
-#                     break
-#             sqs.append(max(new_sq_1, new_sq_2))
-#         return max(sqs)
-# # TEST
-# s = Solution()
-# print(s.maxArea([2,3,4,5,18,17,6]))
+# SOLUTION:
+height = [1,8,6,2,5,4,8,3,7]
 
-
-# Alternative solution (not final yet! it does not work!)
-list = [2,3,4,5,18,17,6]
-
-index = 0
+index_max = len(height)-1
 s = []
-for i, k in enumerate(list):
-    while i > index and list[i] > list[index]:
-        index = index + 1
-    max_sq_1 = k * (i - index)
-    s.append(max_sq_1)
 
-index = len(list)-1  # 6
-for i, k in enumerate(reversed(list)):
-    while len(list)-i-1 < index and list[i] > list[index]:
-        index = index - 1
-    max_sq_2 = k * (index - i)
-    s.append(max_sq_2)
+# For each vertical line we find the one on the right side, which is higher
+for i, k in enumerate(height):
+    while height[index_max] < k and index_max > i:
+        index_max = index_max-1
+    max_sq = k*(index_max - i)
+    s.append(max_sq)
+
+# We do the same for the lines on the left side
+height.reverse()
+index_max = len(height)-1
+
+for i, k in enumerate(height):
+    while height[index_max] < k and index_max > i:
+        index_max = index_max-1
+    max_sq_r = k*(index_max - i)
+    s.append(max_sq_r)
+
 print(max(s))
